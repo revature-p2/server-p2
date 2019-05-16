95f7067068288e0c7d124047d30cfc958750eb8b
David Nelson <49167208+dernestnelson@users.noreply.github.com>
2019-05-16 12:04:51 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,20 +7,25 @@ var spawn   = require('child_process').spawn;
 
 app.use(express.static(__dirname));
 
+//renders the html page
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+//input button submits form "/runscript" and activates the commands in spawn() 
 app.get('/runscript', function(req, res) {
   var command = spawn(__dirname + '/testscript.sh', [req.query.textField]);
   var output  = [];
 
   command.stdout.on('data', function(chunk) {
+      //I console.loged this below and it returned '1'. Not sure why...
     output.push(chunk);
   });
+
+  //I believe this is where the real action occurs
 command.on('close', function(code) {
     if (code === 0)
-      res.send(Buffer.concat(output));
+      res.send(Buffer.concat(output)); //console.loged this and it showed a lot of stuff... 
     else
       res.send(500); // when the script fails, generate a Server Error HTTP response
   });
